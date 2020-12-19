2eb4a583d1a286fde8f3eb0eefed235e30e0840b
André Luís Luza <luza.andre@gmail.com>
2020-12-19 16:19:38 -0300

    new interpretation code

--- R/Interpretation.R
+++ R/Interpretation.R
@@ -3,6 +3,102 @@
 # load packages
 source ("./R/packages.R")
 
+# relationship between empirical and simulated data sets
+
+
+
+plot(rao$FunRao~ses.mpd$mpd.obs.z)
+plot(mean.rao.BM~ses.mpd$mpd.obs.z)
+plot(SES_BM~ses.mpd$mpd.obs.z)
+
+
+mean.rao.EB<-apply(RESULTADOS.RAO.EB,1,mean)
+sd.rao.EB<-apply(RESULTADOS.RAO.EB,1,sd)
+
+cor(rao$FunRao,mean.rao.EB)
+ES_EB<-rao$FunRao - mean.rao.EB
+SES_EB<- ES_BM / sd.rao.EB
+
+plot(rao$FunRao,ses.mpd$mpd.obs.z)
+plot(mean.rao.BM,ses.mpd$mpd.obs.z)
+plot(SES_BM,ses.mpd$mpd.obs.z)
+plot(mean.rao.EB,ses.mpd$mpd.obs.z)
+plot(SES_EB,ses.mpd$mpd.obs.z)
+
+
+# 
+RESULTADOS.RAO.OU
+mean.rao.OU<-apply(RESULTADOS.RAO.OU,1,mean)
+sd.rao.OU<-apply(RESULTADOS.RAO.OU,1,sd)
+
+cor(rao$FunRao,mean.rao.OU)
+ES_OU<-rao$FunRao - mean.rao.OU
+SES_OU<- ES_BM / sd.rao.OU
+
+plot(rao$FunRao~ses.mpd$mpd.obs.z)
+points(mean.rao.BM~ses.mpd$mpd.obs.z,col="red")
+plot(SES_BM~ses.mpd$mpd.obs.z)
+points(mean.rao.EB~ses.mpd$mpd.obs.z,col="blue")
+plot(SES_EB~ses.mpd$mpd.obs.z)
+points(mean.rao.OU~ses.mpd$mpd.obs.z,col="green")
+plot(SES_OU~ses.mpd$mpd.obs.z)
+
+plot(rao$FunRao~ses.mpd$mpd.obs)
+points(mean.rao.BM~ses.mpd$mpd.obs,col="red")
+plot(SES_BM~ses.mpd$mpd.obs)
+points(mean.rao.EB~ses.mpd$mpd.obs,col="blue")
+plot(SES_EB~ses.mpd$mpd.obs)
+points(mean.rao.OU~ses.mpd$mpd.obs,col="green")
+plot(SES_OU~ses.mpd$mpd.obs)
+
+
+
+################################################################################
+#### SES Simulados ####
+SES_NULO # SES NULO com o Atributo Observado
+SES_NULO_BM<-(mean.rao.BM - mean.rao.NULO) / sd.rao.NULO # SES NULO com o Atributo BM
+SES_NULO_EB<-(mean.rao.EB - mean.rao.NULO) / sd.rao.NULO # SES NULO com o Atributo BM
+SES_NULO_OU<-(mean.rao.OU - mean.rao.NULO) / sd.rao.NULO # SES NULO com o Atributo BM
+
+plot(SES_NULO~ses.mpd$mpd.obs.z,ylim=c(-10,4))
+points(SES_NULO_BM~ses.mpd$mpd.obs.z,col="blue")
+points(SES_NULO_EB~ses.mpd$mpd.obs.z,col="red")
+points(SES_NULO_OU~ses.mpd$mpd.obs.z,col="green")
+
+plot(SES_NULO~ses.mpd$mpd.obs,ylim=c(-10,4))
+points(SES_NULO_BM~ses.mpd$mpd.obs,col="blue")
+points(SES_NULO_EB~ses.mpd$mpd.obs,col="red")
+points(SES_NULO_OU~ses.mpd$mpd.obs,col="green")
+
+
+
+# test of whether slope of the regression between ses disparity and ses MPD
+# varies across evolutionary models
+
+
+
+
+
+
+
+
+# exploring phylogenetic uncertainty
+
+
+
+
+
+
+
+# mapping deviations of empirical disparity vs. simulated disparity 
+# across models
+
+
+
+
+
+
+
 # -----------------------------------------------#
 #           Load spatial data
 #       Incidence of 228 species in 1770 cells
