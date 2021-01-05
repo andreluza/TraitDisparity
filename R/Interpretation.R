bf87b5311d2a4c346059f8d93875526c48effbf6
André Luís Luza <luza.andre@gmail.com>
2021-01-05 11:21:51 -0300

    interpretation, analysis considering species with phylogenetic, trait, and occurrence data

--- R/Interpretation.R
+++ R/Interpretation.R
@@ -13,49 +13,6 @@ load (here("output", "RAO_OBS.RData"))
 
 # relationship between empirical and simulated data sets
 
-lapply(seq(1,100), function (i) {
-plot(NA, ylim=c(-7,7),xlim=c(-7,4),
-     xlab=expression (paste("Mean Pairwise Phylogenetic Distance", " (MPD"['SES'],")")),
-     ylab=expression (paste("Morphological Disparity", "  (Disparity"['SES'],")")))
-points(null.mpdf[[i]]$SES.MPD, RAO_OBS$SES, 
-     col=rgb(red=0.3,green=0.3,blue=0.3, alpha=0.1),
-     pch=19)
-abline(lm(RAO_OBS$SES ~null.mpdf[[i]]$SES.MPD),
-       col=rgb(red=0.3,green=0.3,blue=0.3, alpha=0.9),
-       lwd=2)
-points(null.mpdf[[i]]$SES.MPD, RAO_BM[[i]]$SES,
-       col=rgb(red=0,green=0.3,blue=0.9, alpha=0.1),
-       pch=19)
-abline(lm(RAO_BM[[i]]$SES ~null.mpdf[[i]]$SES.MPD),
-       col=rgb(red=0,green=0.3,blue=0.9, alpha=0.9),
-       lwd=2)
-points(null.mpdf[[i]]$SES.MPD, RAO_EB[[i]]$SES,
-       col=rgb(red=1,green=0,blue=0, alpha=0.1),
-       pch=19)
-abline(lm(RAO_EB[[i]]$SES ~null.mpdf[[i]]$SES.MPD),
-       col=rgb(red=1,green=0,blue=0, alpha=0.9),
-       lwd=2)
-points(null.mpdf[[i]]$SES.MPD, RAO_OU[[i]]$SES,
-       col=rgb(red=0.1,green=0.9,blue=0.1, alpha=0.1),
-       pch=19)
-abline(lm(RAO_OU[[i]]$SES ~null.mpdf[[i]]$SES.MPD),
-       col=rgb(red=0.1,green=0.9,blue=0.1, alpha=0.9),
-       lwd=2)
-
-legend ("bottomright", 
-        legend = c("Empirical", "BM","EB","OU"),
-        bty="n",
-        pch=19,
-        col = c(rgb(red=0.3,green=0.3,blue=0.3, alpha=0.7),
-                rgb(red=0,green=0.3,blue=0.9, alpha=0.9),
-                rgb(red=1,green=0,blue=0, alpha=0.9),
-                rgb(red=0.1,green=0.9,blue=0.1, alpha=0.9)
-                
-        ))
-
-
-})
-
 # working with the average
 
 avBM<-do.call(cbind,sapply(RAO_BM, "[","SES",simplify=T))
@@ -63,33 +20,30 @@ avEB<-do.call(cbind,sapply(RAO_EB, "[","SES",simplify=T))
 avOU<-do.call(cbind,sapply(RAO_OU, "[","SES",simplify=T))
 avMPD<-do.call(cbind,sapply(null.mpdf, "[","SES.MPD",simplify=T))
 
-plot(NA, ylim=c(-6,2),xlim=c(-6.5,2.5),
-     xlab=expression (paste("Mean Pairwise Phylogenetic Distance", " (MPD"['SES'],")")),
-     ylab=expression (paste("Morphological Disparity", "  (Disparity"['SES'],")")))
-points(rowMeans (avMPD), RAO_OBS$SES, 
-       col=rgb(red=0.3,green=0.3,blue=0.3, alpha=0.1),
-       pch=19)
-points(rowMeans (avMPD), rowMeans(avBM),
-       col=rgb(red=0,green=0.3,blue=0.9, alpha=0.1),
-       pch=19)
-points(rowMeans (avMPD), rowMeans(avEB),
-       col=rgb(red=1,green=0,blue=0, alpha=0.1),
-       pch=19)
-points(rowMeans (avMPD), rowMeans(avOU),
-       col=rgb(red=0.1,green=0.9,blue=0.1, alpha=0.1),
-       pch=19)
+# df average disparity
+av_disp <- rbind(
+  data.frame (value=RAO_OBS$SES,Data="Empirical"),
+  data.frame (value=rowMeans (avBM),Data="BM"),
+  data.frame (value=rowMeans (avEB),Data="EB"),
+  data.frame (value=rowMeans (avOU),Data="OU"))
+  
+# bind MPD
+av_disp <- cbind(av_disp, MPD = rowMeans (avMPD))
 
-legend ("bottomright", 
-        legend = c("Empirical", "BM","EB","OU"),
-        bty="n",
-        pch=19,
-        col = c(rgb(red=0.3,green=0.3,blue=0.3, alpha=0.7),
-                rgb(red=0,green=0.3,blue=0.9, alpha=0.9),
-                rgb(red=1,green=0,blue=0, alpha=0.9),
-                rgb(red=0.1,green=0.9,blue=0.1, alpha=0.9)
-                
-        ))
-
+#
+figure1 <- ggplot (data = av_disp, aes (x=MPD, y=value,colour=Data)) + 
+  geom_point(aes (col=Data),alpha=0.4,size=1.5) + 
+  stat_smooth(method = "lm", formula = y ~ x, se = FALSE) + 
+  theme_classic() + 
+  scale_colour_manual(values=c("Empirical"="#9dab86", 
+                               "BM"="#21209c",
+                               "EB"="#fdb827",
+                               "OU"="#23120b")) + 
+  xlab("SES of Mean pairwise distance") + 
+  ylab ("SES of Morphological Disparity") + 
+  theme (
+    legend.position = "none",
+    plot.margin=unit(c(.2,1,.1,1),"cm"))
 
 # test of whether slope of the regression between ses disparity and ses MPD
 # varies across evolutionary models
@@ -137,34 +91,37 @@ model_slope <- lapply (bind_data_disparity, function (i) {
             OU=m1$coefficients[8]#,
             #R2 = RsquareAdj(m1)$adj.r.squared
             ) # OU
+  
+  # load also the model results
+  res <- list (coef = res,
+               model = m1)
   ; # return
   res
   
 })
 
 # transforming res list into df
-model_slope_df <- do.call (rbind, model_slope)
+model_slope_df <- do.call (rbind, sapply (model_slope,"[","coef"))
 
 # finally melt
-require(reshape)
-require(ggplot2)
+
 model_slope_df <- melt(model_slope_df)
 
 # plotting
 
-a <- ggplot(model_slope_df, aes(x=value, color=variable, fill=variable)) +
+fig2 <- ggplot(model_slope_df, aes(x=value, color=variable, fill=variable)) +
   geom_density(size=1,alpha=0.1) +
-  scale_fill_manual(values=c("EMPIRICAL"="gray", 
-                             "BM"="blue",
+  scale_fill_manual(values=c("EMPIRICAL"="#9dab86", 
+                             "BM"="#21209c",
                              "EB"="red",
-                             "OU"="green")) + 
-  scale_colour_manual(values=c("EMPIRICAL"="gray", 
-                                        "BM"="blue",
-                                        "EB"="red",
-                                        "OU"="green"))+
+                             "OU"="#23120b")) + 
+  scale_colour_manual(values=c("EMPIRICAL"="#9dab86", 
+                                        "BM"="#21209c",
+                                        "EB"="#fdb827",
+                                        "OU"="#23120b"))+
   theme_classic()  
 
-b <- a+ theme (
+fig2a <- fig2+ theme (
                axis.text.x = element_text(angle = 90),
                legend.title = element_blank(),
                #legend.position = "none",
@@ -176,80 +133,135 @@ b <- a+ theme (
   xlab ("Slope SES Disparity ~ SES MPD") + 
   ylab("Density") + 
   scale_x_continuous(breaks = seq(-2,2,0.15),
-                     limits = c(-1.5,1.5)) 
+                     limits = c(-1.5,1.5))
 
 # exploring phylogenetic uncertainty
 
-b
+boxplotOver <- ggboxplot(model_slope_df, x = "variable", y = "value",
+               color = "variable", palette =c("#9dab86", 
+                                              "#21209c",
+                                              "#fdb827",
+                                              "#23120b"),
+               fun = "mean_ci", add.params = list(alpha=0.2),size=0.5)
+
+boxplotOver1 <- boxplotOver + theme(axis.line=element_blank(),
+                axis.text.x=element_blank(),
+                axis.text.y=element_blank(),
+                axis.ticks=element_blank(),
+                axis.title.x=element_blank(),
+                axis.title.y=element_blank(),
+                legend.position="none",
+                panel.background=element_blank(),
+                panel.border=element_blank(),
+                panel.grid.major=element_blank(),
+                panel.grid.minor=element_blank(),
+                plot.background=element_blank(),
+                plot.margin=unit(c(.2,1,.1,2),"cm")) +
+  scale_y_continuous(limits=c(-1.2, 1.2)) + coord_flip() 
 
 
+#pdf (paste ("compTR",variaveis[i],".pdf",sep=""), width=5,height=4,family="serif")
+panel <- grid.arrange(figure1,
+              boxplotOver1,fig2a,
+             ncol=4,nrow = 9,
+             layout_matrix = rbind (c(1,1,1,1),
+                                    c(1,1,1,1),
+                                    c(1,1,1,1),
+                                    c(1,1,1,1),
+                                    c(2,2,2,2),
+                                    c(3,3,3,3),
+                                    c(3,3,3,3),
+                                    c(3,3,3,3),
+                                    c(3,3,3,3)))
+
+#dev.off()
 
 
+## Fstatistics
 
+F_stat <- lapply(sapply(model_slope,'[',"model"), function (i) summary (i)$fstatistic)
+apply(do.call(rbind, F_stat),2,mean)
+apply(do.call(rbind, F_stat),2,sd)
 
+# R2 adj
+R2_stat <- lapply(sapply(model_slope,'[',"model"), function (i) summary (i)$adj.r.squared)
+mean(unlist(R2_stat))
+sd(unlist(R2_stat))
 
+## average results of the models
 
-# mapping deviations of empirical disparity vs. simulated disparity 
-# across models
+coef_table <- lapply(sapply(model_slope,'[',"model"), function (i) summary (i)$coefficients)
+## mean
+list.mean <- Reduce("+", coef_table) / length(coef_table)
+## squared mean
+list.squared.mean <- Reduce("+", lapply(coef_table, "^", 2)) / length(coef_table)
+## variance
+list.variance <- list.squared.mean - list.mean^2
+## standard deviation
+list.sd <- sqrt(list.variance)
 
-
-
-
-
-
-
-# -----------------------------------------------#
-#           Load spatial data
-#       Incidence of 228 species in 1770 cells
+# -------------------------------------------------------------------- #
+# Mapping deviations of empirical disparity vs. simulated disparity 
+# across models#
+#
+## -----------------------------------------------#
+##           Load spatial data
+##       Incidence of 228 species in 1770 cells
 
 presab_original <-read.table(here ("data","PresAbs_228sp_Neotropical_MainDataBase_Ordenado.txt"),h=T)
+presab_original <- presab_original [order(as.numeric(rownames(presab_original))),]
 # remove sites with 2 or less sp
 presab <- presab_original [rowSums (presab_original) > 3,]
 ## exclude species absent in the remaining cells
 presab <- presab [,which(colSums(presab)>0)]
+presab <- presab[order(as.numeric(rownames(presab)),decreasing=F),]
 
 # ----------------------------- # 
 ##  Geographic coordinates data
 
 longlat<-read.table(here ("data","Lon-lat-Disparity.txt"),h=T)
-#longlat  <- longlat [rowSums (presab_original) > 3,]
+# rm sites with efw spp
+longlat  <- longlat [,1:2]
+longlat <- longlat[order(as.numeric((longlat$LONG)),decreasing=F),]
+longlat <- longlat[order(as.numeric((longlat$LAT)),decreasing=F),]
+longlat <- longlat [rowSums (presab_original) > 3,]
 
-library(sp)
-library(raster)
-library (rgdal)
+table(rownames(presab) == rownames(longlat))
 
-coord_data <- longlat[,c("LONG","LAT")]
+# data to MAP
 
-coordinates (coord_data)<- ~ LONG+LAT
+data_to_map <- data.frame(longlat,
+                          #Richness=rowSums(presab),
+                          'EMPIRICAL'= RAO_OBS$SES,
+                          'BROWNIAN MOTION'=rowMeans (avBM),
+                          'EARLY BURST' =rowMeans (avEB),
+                          'ORNSTEIN-UHLENBECK'=rowMeans (avOU)
+                          #'NEUTRAL' = RAO_OBS$SES-rowMeans (avEB)
+                          #MPD=rowMeans (avMPD)
+)
 
-# load shapafile of neotropics
-neotropics <- readOGR(dsn=here ("data","Lowenberg_Neto_2014_shapefile"),layer="Lowenberg_Neto_2014")
-neotropics <- spTransform (neotropics,CRS=CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"))
-# objects in the same proj
-coord_data <- spTransform(coord_data,crs(neotropics))
-# make grid over neotropics
-grdpts <- makegrid(neotropics, cell=1)
-# turn into ponts
-spgrd <- SpatialPoints(grdpts, proj4string = CRS(proj4string(neotropics)))
-# into pixels
-spgrdWithin <- SpatialPixels(spgrd[neotropics,])
-# and transform into grids
-spgrdWithin <- as(spgrdWithin, "SpatialGrid")
+#write.csv(data_to_map,here("output","data_to_SAM.csv"))
 
-# transforming into raster
-r<-(raster(spgrdWithin))
-r[]<-1:ncell(r)
+melt_data_to_map <- melt(data_to_map,id=c("LONG","LAT"))
+colnames(melt_data_to_map)[which(colnames(melt_data_to_map) == "value")] <- 'SES'
 
-# plot
-plot(r)
-plot(neotropics,add=T)
+## empirical vs simulated
+#plot using ggplot
+map1 <- ggplot(melt_data_to_map, 
+               aes(x = LONG, y = LAT)) +
+  geom_tile(aes(fill = SES)) +
+  facet_wrap(~variable,scales = "fixed",ncol=2)+
+  scale_fill_gradient2(midpoint = 0, mid="#eee8d5", high="#dc322f", low="#268bd2") + 
+  theme_classic() + 
+  theme_map() +
+  xlab("Longitude") + ylab("Latitude")+
+  theme(legend.position="right",
+        legend.justification = "center",
+        legend.direction = "vertical",
+        panel.spacing = unit(1, "lines"),
+        strip.text = element_text(size=10))
+map1
 
-plot(spgrdWithin, add = T)
-points(coord_data,pch=19,cex=0.1,col="red")
+ggsave (here("output","maps.pdf"),height = 6,width=5)
 
-# mask based on coord values
-subset_neo <- mask (r,coord_data )
-subset_neo[] <- longlat$SES.SIZE.DISPARITY
-plot(subset_neo)
 
-points(coord_data,pch=19,cex=0.1,col="red")
